dab3b84124f4d2097ac4cb62f25bbd44eb4d991f
Shawn Le <aceofclubs54@gmail.com>
2020-07-29 21:22:20 -0700

    Add initial Hello World materials!
    For more info: https://www.graphql-java.com/tutorials/getting-started-with-spring-boot/

---
+++ src/main/java/com/shawnle007/bytes/graphqlbytes/GraphqlbytesApplication.java
@@ -0,0 +1,13 @@
+package com.shawnle007.bytes.graphqlbytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlbytesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlbytesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shawnle007/bytes/graphqlbytes/GraphqlbytesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawnle007.bytes.graphqlbytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlbytesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
